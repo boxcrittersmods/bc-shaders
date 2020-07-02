2db546bba2a175d3a9eba7a9e377fdd6a7b47150
Tumble <tumblegamer@gmail.com>
2020-07-02 10:27:31 +0100

    version

--- bc-shaders.user.js
+++ bc-shaders.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BoxCritters Shaders
 // @namespace    https://boxcrittersmods.ga/
-// @version      0.59
+// @version      0.0.1.60
 // @description  Create shaders for boxcritters
 // @author       TumbleGamer, SArpnt
 // @match        https://boxcritters.com/play/
